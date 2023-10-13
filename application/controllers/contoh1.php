33add5270b1705b2b690a4785da548e197aa90dd
rckyjnitra <ricjy.janitra@gmail.com>
2023-10-13 15:03:38 +0700

    mengumpulkan tugas wp2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Ricky Janitra Saya Tinggal di daerah Jaktim olahraga yang saya sukai adalah Badminton";
+        
+    }
+
+}
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk Kita Belajar Web Programming..!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan method penjumlahan yaitu <?= 
+    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
+
